29eb53521229690a6d3977bbfa0b17e6b0157312
PeterC <petercalifano.gs@gmail.com>
2024-05-14 11:18:58 +0200

    Added examples and concepts

---
+++ PyTorch/examples/imageSegmentation.py
@@ -0,0 +1,11 @@
+# Script implementing the  example as exercise by PeterC - DD-05-2024
+# Reference: 
+
+# Import modules
+import torch
+from torch import nn
+from torch.utils.data import DataLoader # Utils for dataset management, storing pairs of (sample, label)
+from torchvision import datasets # Import vision default datasets from torchvision
+from torchvision.transforms import ToTensor # Utils
+import datetime
+import numpy as np
