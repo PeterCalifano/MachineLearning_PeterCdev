5843d1f11a9958397a8932da666b4df1b3d9d6bb
PeterC <petercalifano.gs@gmail.com>
2024-06-10 16:43:33 +0200

    First prototype of testing script completed

--- matlab/LimbBasedNavigationAtMoon/LimbPixelsEnhancerCNN_Testing.m
+++ matlab/LimbBasedNavigationAtMoon/LimbPixelsEnhancerCNN_Testing.m
@@ -2,9 +2,9 @@ close all
 clear
 clc
 
-
-addpath('..')
-
+addpath('..');
+addpath('testModels\');
+addpath(genpath('testDatapairs\'));
 % SCRIPT NAME
 % LimbPixelsEnhancerCNN_Testing
 % -------------------------------------------------------------------------------------------------------------
@@ -19,6 +19,7 @@ addpath('..')
 % -------------------------------------------------------------------------------------------------------------
 % CHANGELOG
 % 09-06-2024    Pietro Califano     Script initialized. Model loading code added.
+% 10-06-2024    Pietro Califano     First version of script completed.
 % -------------------------------------------------------------------------------------------------------------
 % DEPENDENCIES
 % Deep Learning toolbox
@@ -26,40 +27,47 @@ addpath('..')
 % Future upgrades
 
 %% OPTIONS
+fileID   = 1;
+sampleID = 1; % Sample patch ID in loaded datapair file
+testDatapairsStruct = getDirStruct(dataJSONpath);
 
-
+dataJSONpath = fullfile(testDatapairsStruct(fileID));
 
 %% Simulation data loading
-dataJSONpath = fullfile();
-datastruct = JSONdecoder(dataJSONpath);
+datastruct = JSONdecoder(dataJSONpath(fileID));
 
 % Get flattened patch
-sampleID = 1
 flattenedWindow  = datastruct.ui8flattenedWindows(:, sampleID);
 coarseLimbPixels = datastruct.ui16coarseLimbPixels(:, sampleID)
 % Validate patch counting how many pixels are completely black or white
 % pathIsValid = customTorch.IsPatchValid(flattenedWindow, lowerIntensityThr=5);
 
+% Improvements I should do to the model:
+% 1) Remove constant input R_DEM. If needed, convert it to apparent pixel size to introduce the concept of
+% "distance" in the network map.
+% 2) Input image should be normalized wrt to the maximum. This tip applies to all the inputs.
+
 % Compose input sample
 inputDataSample = zeros(60, 1, 'single');
 
-inputDataSample(1:49)   = single(flattenedWindow);
-inputDataSample(50)     = single(datastruct.dRmoonDEM);
-inputDataSample(51:52)  = single(datastruct.dSunDir_PixCoords);
-inputDataSample(53:55)  = single(quat2mrp( DCM2quat(datastruct.dAttDCM_fromTFtoCAM, false) )); % Convert Attitude matrix to MRP parameters
-inputDataSample(56:58)  = single(datastruct.dPosCam_TF);
-inputDataSample(59:60)  = single(coarseLimbPixels);
+inputDataSample(1:49)  = single(flattenedWindow); 
+inputDataSample(50)    = single(datastruct.dRmoonDEM);
+inputDataSample(51:52) = single(datastruct.dSunDir_PixCoords);
+inputDataSample(53:55) = single(quat2mrp( DCM2quat(reshape(datastruct.dAttDCM_fromTFtoCAM, 3, 3), false) )); % Convert Attitude matrix to MRP parameters
+inputDataSample(56:58) = single(datastruct.dPosCam_TF);
+inputDataSample(59:60) = single(coarseLimbPixels);
 
 %% Model loading
-path2model;
-trainedCNN = ImportModelFromONNx(path2model, 'regression');
+path2model = 'testModels';
+modelName = 'traineModel_0';
+trainedCNN = ImportModelFromONNx(fullfile(path2model, modelName), 'regression');
 
 %% Model evaluation     
 
 % NOTES: 
 % 1) Check which shape the model from ONNx will require --> MATLAB converts the model as it is. 
-%    Therefore it will requires the same shape as in Torch code.
-% 2) Which dtype? --> Python should use float (?) --> Apparently yes, try single
+%    Therefore it will require the same shape as in Torch code.
+% 2) Which dtype? --> Python should use float (?) --> Apparently yes, try single()
 
 outputPrediction = predict(trainedCNN, reshape(inputDataSample, 1, 60) );
 
