5a6aff653c9f8c2bbce3c1a289c242b7aec0982b
PeterC <petercalifano.gs@gmail.com>
2024-06-15 15:39:57 +0200

    Added instantiations of tcpserver objects

--- PyTorch/torchModelOverTCP.py
+++ PyTorch/torchModelOverTCP.py
@@ -10,30 +10,40 @@ sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/tcpS
 sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch'))
 sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/LimbBasedNavigationAtMoon'))
 
+import numpy as np
+
 # Custom imports
 import customTorch
 import limbPixelExtraction_CNN_NN
 import tcpServerPy
 
 
+
 # MAIN SCRIPT
 def main():
-    print('MAIN script operations: initialize always-on server --> listen to data from client --> if OK, evaluate model --> if OK, return output to client')
+    print('\n\n--------------------------- RUNNING: torchModelOverTCP.py ---------------------------n')
+    print('MAIN script operations: initialize always-on server --> listen to data from client --> if OK, evaluate model --> if OK, return output to client\n\n')
     
     # %% TORCH MODEL LOADING
     # Model path
     tracedModelSavePath = '/home/peterc/devDir/MachineLearning_PeterCdev'
-    tracedModelName = 'trainedModel_' + customTorch.AddZerosPadding(0, 4)
+    tracedModelName = 'trainedTracedModel' + customTorch.AddZerosPadding(75, 3)
 
     # Parameters
-    device = customTorch.GetDevice()
 
     # Load torch traced model from file
-    trainedTracedModel = customTorch.LoadTorchModel(None, tracedModelName, tracedModelSavePath, True).to(device)
+    torchWrapper = customTorch.TorchModel_MATLABwrap(tracedModelName, tracedModelSavePath)
 
     # %% TCP SERVER INITIALIZATION
+    HOST, PORT = "localhost", 65333 # Define host and port (random is ok)
 
+    # Define DataProcessor object for RequestHandler
+    dataProcessorObj = tcpServerPy.DataProcessor(torchWrapper.forward, np.ndarray)
 
+    # Initialize TCP server and keep it running
+    with tcpServerPy.pytcp_server((HOST, PORT), tcpServerPy.pytcp_requestHandler, bindAndActivate=True) as server:
+        print('Server initialized correctly. Set in "serve_forever" mode.')
+        #server.serve_forever()
 
 if __name__ == "__main__":
     main()
