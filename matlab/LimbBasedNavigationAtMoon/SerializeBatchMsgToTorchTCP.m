0e5f3185fa13563d7583ee57f05eeff938951af6
PeterC <petercalifano.gs@gmail.com>
2024-06-24 23:09:17 +0200

    Major upgrade for batch input serialization; function is now agnostic to input data. Order is determined by fields order.

--- matlab/LimbBasedNavigationAtMoon/SerializeBatchMsgToTorchTCP.m
+++ matlab/LimbBasedNavigationAtMoon/SerializeBatchMsgToTorchTCP.m
@@ -1,22 +1,23 @@
 function [dataLength, dataBufferToWrite] = SerializeBatchMsgToTorchTCP(i_strInputSamplesBatchStruct, ...
-                                                                       i_ui32Nbatches)%#codegen
+                                                                       i_ui32Nbatches, ...
+                                                                       i_ui32InputSize)
 arguments
-    i_ui32Nbatches               (
-    i_strInputSamplesBatchStruct (i_ui32Nbatches, :) 
-    
+    i_strInputSamplesBatchStruct (:,:) {isstruct}
+    i_ui32Nbatches               (1,1) uint32
+    i_ui32InputSize              (1,1) uint32
 end
 %% PROTOTYPE
-% [dataLength, dataBufferToWrite] = SerializeMsgToTorchTCP(i_ui8flattenedWindow, ...
-%     i_dSunDir_PixCoords, ...
-%     i_dAttMRP_fromTFtoCAM, ...
-%     i_ui8coarseLimbPixels)
+% [dataLength, dataBufferToWrite] = SerializeBatchMsgToTorchTCP(i_strInputSamplesBatchStruct, ...
+%                                                                        i_ui32Nbatches, ...
+%                                                                        i_ui32InputSize)
 % -------------------------------------------------------------------------------------------------------------
 %% DESCRIPTION
 % What the function does
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
-% in1 [dim] description
-% Name1                     []
+% i_strInputSamplesBatchStruct (:,:) {isstruct}
+% i_ui32Nbatches               (1,1) uint32
+% i_ui32InputSize              (1,1) uint32
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
 % out1 [dim] description
@@ -25,7 +26,9 @@ end
 %% CHANGELOG
 % 17-06-2024        Pietro Califano         Adapted from script.
 % 20-06-2024        Pietro Califano         Update for integration with LUMIO IP
-% 24-06-2024        Pietro Califano         
+% 24-06-2024        Pietro Califano         Upgrade to serialize batch of input samples; assignment is now
+%                                           agnostic wrt the input data. Order has to be managed in the
+%                                           generation of the input struct (same as order in input sample).
 % -------------------------------------------------------------------------------------------------------------
 %% DEPENDENCIES
 % [-]
@@ -34,6 +37,7 @@ end
 % [-]
 % -------------------------------------------------------------------------------------------------------------
 %% Function code
+
 % ui8flattenedWindow
 % dRmoonDEM --> REMOVED
 % dSunDir_PixCoords
@@ -41,38 +45,59 @@ end
 % dPosCam_TF --> REMOVED
 % ui8coarseLimbPixels
 
-inputDataSample = zeros(56, 1, 'single');
-ptrToInput = 1;
+% NOTE: Fields must be in this order unless some other criterion is used to specify how to serialize the
+% data and let the receiver know how to deserialize. There must be an agreement in any case. Otherwise one
+% end has to send this information. No workaround for this.
+% i_ui8flattenedWindow
+% i_dSunDir_PixCoords
+% i_dAttMRP_fromTFtoCAM
+% i_ui8coarseLimbPixels
 
-flattenedWindSize = length(i_ui8flattenedWindow);
+inputDataBatch = zeros(i_ui32InputSize, i_ui32Nbatches, 'single');
 
-inputDataSample(ptrToInput:ptrToInput+flattenedWindSize-1)  = single(i_ui8flattenedWindow);
+% Get fieldnames and field sizes
+fieldNamesCell = fieldnames(i_strInputSamplesBatchStruct(1));
+fieldSizes = zeros(length(fieldNamesCell), 1);
 
-% Update index
-ptrToInput = ptrToInput + flattenedWindSize; 
-
-inputDataSample(ptrToInput:ptrToInput+length(i_dSunDir_PixCoords)-1) = single(i_dSunDir_PixCoords);
-
-% Update index
-ptrToInput = ptrToInput + length(i_dSunDir_PixCoords); % Update index
-
-
-inputDataSample(ptrToInput : ptrToInput + length(i_dAttMRP_fromTFtoCAM)-1) = single(i_dAttMRP_fromTFtoCAM); % Convert Attitude matrix to MRP parameters
-
-% Update index
-ptrToInput = ptrToInput + length(i_dAttMRP_fromTFtoCAM); % Update index
-
-inputDataSample(ptrToInput:end) = single(i_ui8coarseLimbPixels);
-
-
-% dataBufferSize = uint16(60*8);
-
-if iscolumn(inputDataSample)
-    dataMessage = transpose(inputDataSample);
-else
-    dataMessage = (inputDataSample);
+for idF = 1:length(fieldNamesCell)
+    fieldSizes(idF) = length(i_strInputSamplesBatchStruct(1).(fieldNamesCell{idF}));
 end
 
+% Assignment loop
+for idB = 1:i_ui32Nbatches
+
+    % Temporary variables
+    tmpSample = zeros(i_ui32InputSize, 1, 'single');
+    ptrToInput = 1;
+
+    % Extract and assign idB input sample
+    for idF = 1:length(fieldNamesCell)-1
+
+        % Store data into chunk of sample
+        tmpSample(ptrToInput:ptrToInput+fieldSizes(idF)-1)  = single(i_strInputSamplesBatchStruct(idB).(fieldNamesCell{idF}));
+
+        % Update index
+        ptrToInput = ptrToInput + fieldSizes(idF);
+
+    end
+
+    % Assign last input using end as assert
+    tmpSample(ptrToInput:end) = single(i_strInputSamplesBatchStruct(idB).(fieldNamesCell{end}));
+
+    % Assign temporary sample into data batch
+    inputDataBatch(:, idB) = tmpSample;
+end
+
+% Reshape data batch to column vector before casting
+inputDataBatch = reshape(inputDataBatch, [], 1);
+
+if iscolumn(inputDataBatch)
+    dataMessage = transpose(inputDataBatch);
+else
+    dataMessage = (inputDataBatch);
+end
+
+% Cast and serialize message string
 dataBuffer = typecast(dataMessage, 'uint8');
 dataLength = typecast(uint32(length(dataBuffer)), 'uint8');
 dataBufferToWrite = [dataLength, dataBuffer];
