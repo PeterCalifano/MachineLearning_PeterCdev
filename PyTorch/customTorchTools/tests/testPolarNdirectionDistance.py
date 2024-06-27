aba503b4e28cc1949f032bbeebd37978aaf18955
PeterC <petercalifano.gs@gmail.com>
2024-06-27 17:35:29 +0200

    Bug fixes

--- PyTorch/customTorchTools/tests/testPolarNdirectionDistance.py
+++ PyTorch/customTorchTools/tests/testPolarNdirectionDistance.py
@@ -4,28 +4,32 @@ Created by PeterC, 26-06-2024. Reference: “[14] Y. Wu, H. Wang, F. Tang, and Z
 ” Pattern Recognition, vol. 90, pp. 415-423, 2019.” 
 '''
 
-import sys
+import sys, os
 # Append paths of custom modules
 sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/customTorchTools'))
 sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/LimbBasedNavigationAtMoon'))
 
 # Import the required modules
-import torch, os
+import torch
 import customTorchTools
 import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 import cv2
 from typing import Union
 
 
 
-def ComputePolarNdirectionDistance(CconicMatrix:Union[np.array | torch.tensor | list], 
-                                   pointCoords: Union[np.array | torch.tensor], device=customTorchTools.getDevice(), self=None):
+def ComputePolarNdirectionDistance(CconicMatrix:Union[np.array , torch.tensor , list], 
+                                   pointCoords: Union[np.array , torch.tensor]):
     '''
     Function to compute the Polar-n-direction distance of a point from a conic in the image plane represented by its [3x3] matrix.
     '''    
     # Convert the input to homogeneous coordinates
-    pointHomoCoords = torch.tensor([pointCoords[0], pointCoords[1], 1], shape=(3,1))
+    pointHomoCoords = torch.tensor([pointCoords[0], pointCoords[1], 1]).reshape(3,1)
 
     # Compute auxiliary variables
     CbarMatrix = torch.zeros((3,3))
@@ -60,44 +64,51 @@ def ComputePolarNdirectionDistance(CconicMatrix:Union[np.array | torch.tensor | 
 
     return sqrDist
 
-def ComputePolarNdirectionDistance_asTensor(CconicMatrix:Union[np.array | torch.tensor | list], 
-                                   pointCoords: Union[np.array | torch.tensor], device=customTorchTools.getDevice(), self=None):
-        
+def ComputePolarNdirectionDistance_asTensor(CconicMatrix:Union[np.array , torch.tensor , list], 
+                                   pointCoords: Union[np.array , torch.tensor]):
+    '''
+    Function to compute the Polar-n-direction distance of a point from a conic in the image plane represented by its [3x3] matrix using torch tensors operations.
+    '''
+
+    device = pointCoords.device
     # Shape point coordinates as tensor
     batchSize = pointCoords.shape[0]
 
-    pointHomoCoords_tensor = torch.zeros(shape=(batchSize,3,1), dtype=torch.float32, device=device)
-    pointHomoCoords_tensor[:, 0:2, 0] = torch.stack(pointCoords[:, 0], pointCoords[:, 1])
+    pointHomoCoords_tensor = torch.zeros((batchSize,3,1), dtype=torch.float32, device=device)
+    pointHomoCoords_tensor[:, 0:2, 0] = torch.stack((pointCoords[:, 0], pointCoords[:, 1]), dim=1)
     pointHomoCoords_tensor[:, 2, 0] = 1
 
     # Reshape Conic matrix to tensor
-    CconicMatrix = CconicMatrix.view(batchSize, 3, 3)
+    CconicMatrix = CconicMatrix.view(batchSize, 3, 3).to(device)
 
     CbarMatrix_tensor = torch.zeros((batchSize, 3, 3), dtype=torch.float32, device=device)
-    CbarMatrix_tensor[:, 0:2,0:2] = CconicMatrix[:, 0:2,0:2]
+    CbarMatrix_tensor[:, 0:2,0:3] = CconicMatrix[:, 0:2,0:3]
 
     Gmatrix_tensor = torch.bmm(CconicMatrix, CbarMatrix_tensor)
     Wmatrix_tensor = torch.bmm(torch.bmm(CbarMatrix_tensor.transpose(1,2), CconicMatrix), CbarMatrix_tensor)
 
     # Compute Gdist2, CWdist and Cdist
-    Cdist_tensor = torch.bmm( torch.bmm(pointHomoCoords_tensor.transpose(1, 2), torch.bmm(CconicMatrix, pointHomoCoords_tensor)) )
+    Cdist_tensor = torch.bmm( pointHomoCoords_tensor.transpose(1, 2), torch.bmm(CconicMatrix, pointHomoCoords_tensor) ) 
     
-    Gdist_tensor = ( torch.bmm( torch.bmm(pointHomoCoords_tensor.transpose(1, 2), torch.bmm(Gmatrix_tensor, pointHomoCoords_tensor))) )
+    Gdist_tensor = torch.bmm( pointHomoCoords_tensor.transpose(1, 2), torch.bmm(Gmatrix_tensor, pointHomoCoords_tensor) )
     Gdist2_tensor = torch.bmm(Gdist_tensor, Gdist_tensor)
     
-    Wdist_tensor = ( torch.bmm(pointHomoCoords_tensor.transpose(1, 2) * torch.bmm(Wmatrix_tensor, pointHomoCoords_tensor)) )
+    Wdist_tensor = torch.bmm(pointHomoCoords_tensor.transpose(1, 2), torch.bmm(Wmatrix_tensor, pointHomoCoords_tensor)) 
     CWdist_tensor = torch.bmm(Cdist_tensor, Wdist_tensor)
 
 
     # Get mask for the condition
     idsMask = Gdist2_tensor >= CWdist_tensor
-    notIdsMask = ~(idsMask)
+
+    print('Any false in idsMask? ', torch.any(idsMask == False).item())
+
+    notIdsMask = Gdist2_tensor < CWdist_tensor
 
     # Compute the square distance depending on if condition
     sqrDist_tensor = torch.zeros(batchSize, dtype=torch.float32, device=device)
 
-    sqrDist_tensor[idsMask] = Cdist_tensor[idsMask] / (Gdist_tensor[idsMask] * ( 1 + torch.sqrt(1 + (Gdist2_tensor[idsMask] - CWdist_tensor[idsMask])/Gdist2_tensor[idsMask]) )**2)
-    sqrDist_tensor[notIdsMask] = 0.25 * (Cdist_tensor[notIdsMask]**2 / Gdist_tensor[notIdsMask])
+    sqrDist_tensor[idsMask[:,0,0]] = Cdist_tensor[idsMask] / ( Gdist_tensor[idsMask] * ( 1 + torch.sqrt(1 + (Gdist2_tensor[idsMask] - CWdist_tensor[idsMask]) / Gdist2_tensor[idsMask]) )**2)
+    sqrDist_tensor[notIdsMask[:,0,0]] = 0.25 * (Cdist_tensor[notIdsMask]**2 / Gdist_tensor[notIdsMask])
 
     return sqrDist_tensor
 
@@ -110,16 +121,11 @@ def main():
 
     # %% TEST: loss function
     # Construct meshgrid over [7x7] patch for evaluation
-    Npoints = 7
-    xSpace = np.linspace(-3, 3, Npoints)
-    ySpace = np.linspace(-3, 3, Npoints)
+    Npoints = 500
+    xSpace = np.linspace(0, 1024, Npoints)
+    ySpace = np.linspace(0, 1024, Npoints)
 
     X, Y   = np.meshgrid(xSpace, ySpace)
-
-    X = torch.tensor(X, dtype=torch.float32, device=customTorchTools.getDevice())
-    Y = torch.tensor(Y, dtype=torch.float32, device=customTorchTools.getDevice())
-
-    pixelCoords = torch.stack((X.flatten(), Y.flatten()), dim=1)
 
     # Define the conic matrix as [9x1] vector
     conicMatrix = torch.tensor([2.0041524863091946E-6,
@@ -130,15 +136,44 @@ def main():
             -0.0010261260729903076,
             -0.0010261260729903081,
             -0.0010261260729903076,
-            1])
+            1]).reshape(1, 3, 3).repeat(Npoints**2, 1, 1)
+    
+    # TEST: For loop version
+    sqrDist_loop = np.zeros((Npoints, Npoints))
+
+    for idX in range(xSpace.size):
+        for idY in range(ySpace.size):
+            sqrDist_loop[idX][idY] = ComputePolarNdirectionDistance(conicMatrix[0,:,:], torch.tensor([xSpace[idX], ySpace[idY]]))
+
+    # TEST: Tensor evaluation version
+
+    X = torch.tensor(X, dtype=torch.float32, device=customTorchTools.GetDevice())
+    Y = torch.tensor(Y, dtype=torch.float32, device=customTorchTools.GetDevice())
+
+    pixelCoords = torch.stack((X.flatten(), Y.flatten()), dim=1)
+
+
 
     # Evaluate the loss function over the meshgrid
-    sqrDist_tensor = ComputePolarNdirectionDistance_asTensor(conicMatrix, pixelCoords, device=customTorchTools.getDevice())
+    sqrDist_tensor = ComputePolarNdirectionDistance_asTensor(conicMatrix, pixelCoords)
 
+    sqrDist_np = np.array(sqrDist_tensor.cpu().detach().numpy()).reshape(Npoints, Npoints).T
 
     # Plot the loss function values
+    Nlevels = 100
 
+    fig, ax = plt.subplots()
 
+    X, Y = np.array(X.cpu().detach().numpy()), np.array(Y.cpu().detach().numpy())
+    ax.contourf(X, Y, sqrDist_np, levels=Nlevels)
+    
+    fig2, ax2 = plt.subplots(subplot_kw={"projection": "3d"})
+    
+    ax2.plot_surface(X, Y, sqrDist_np**2, cmap='viridis', edgecolor='none')
+
+    plt.show(block=True)
+    
+    
 
 
 if __name__ == "__main__":
