9ff3d9b9b191e6f743e0319dec3c80a45e7f185b
PeterC <petercalifano.gs@gmail.com>
2024-06-30 11:57:46 +0200

    Added doc comment

--- PyTorch/customTorchTools/customTorchTools.py
+++ PyTorch/customTorchTools/customTorchTools.py
@@ -1,5 +1,11 @@
-# Module collecting utilities function building upon PyTorch to speed up prototyping, training and testing of Neural Nets 
-# Created by PeterC - 04-05-2024
+'''
+Module collecting utilities function building upon PyTorch to speed up prototyping, training and testing of Neural Nets.
+Utilities for monitoring training, validation and testing of models, as well as saving and loading models and datasets are included.
+The module also includes functions to export and load models to/from ONNx format, as well as a MATLAB wrapper class for model evaluation.
+NOTE: This however, requires TorchTCP module (not included yet) and a MATLAB tcpclient based interface.
+Created by PeterC - 04-05-2024. Current version: v0.1 (30-06-2024)
+'''
+
 
 # Import modules
 import torch
