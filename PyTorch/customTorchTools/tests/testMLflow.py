cac35b2da1b5309dee2e4060f7f1e8b135a8b753
PeterC <petercalifano.gs@gmail.com>
2024-07-02 13:03:06 +0200

    Added new submodule for mlflow and test script

---
+++ PyTorch/customTorchTools/tests/testMLflow.py
@@ -0,0 +1,31 @@
+''' Script created by PeterC to test mlflow library for model monitoring and tracking - 02-07-2024 '''
+import onnx 
+import torch
+from torch import nn 
+
+# Import modules
+import sys, os
+# Append paths of custom modules
+#sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch'))
+sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/LimbBasedNavigationAtMoon'))
+
+import customTorchTools # Custom torch tools
+import numpy as np
+import mlflow
+
+
+
+def mlflowTracking():
+
+
+
+def main():
+    print('TEST SCRIPT: MLflow functionalities')
+    # %% MLflow tracking
+
+
+
+
+
+if __name__ == "__main__":
+    main()
