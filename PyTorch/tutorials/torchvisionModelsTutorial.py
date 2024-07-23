ec1f4093effbbf9322d14a599a5da4c3a3b4ed8a
PeterC <petercalifano.gs@gmail.com>
2024-07-23 17:24:28 +0200

    Renamed scripts, added tutorials to do

---
+++ PyTorch/tutorials/torchvisionModelsTutorial.py
@@ -0,0 +1,4 @@
+'''
+Script implementing code from PyTorch tutorial about torchvision pretrained models and classes, PeterC, 23-07-2024.
+Reference: https://pytorch.org/vision/stable/models.html
+'''
---
+++ PyTorch/tutorials/transferLearningTutorial.py
@@ -0,0 +1,4 @@
+'''
+Script implementing code from PyTorch tutorial about transfer learning for CV models, PeterC, 23-07-2024.
+Reference: https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+'''
