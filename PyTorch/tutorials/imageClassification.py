db22462619a3993b12296216ccba71983f4a0674
PeterC <petercalifano.gs@gmail.com>
2024-07-23 17:16:07 +0200

    Added code to test classification task with customTorchTools and modified torchvision model

--- PyTorch/tutorials/imageClassification.py
+++ PyTorch/tutorials/imageClassification.py
@@ -7,13 +7,18 @@ from torch import nn
 from torch.utils.data import DataLoader # Utils for dataset management, storing pairs of (sample, label)
 from torchvision import datasets # Import vision default datasets from torchvision
 from torchvision.transforms import ToTensor # Utils
-
+from torchvision import transforms
+import mlflow
 import numpy as np
 
 import sys, os
 sys.path.append(os.path.join('/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/customTorchTools'))
 import customTorchTools
 import torchvision.models as models
+
+wsDir = '/home/peterc/devDir/MachineLearning_PeterCdev/PyTorch/tutorials'
+if os.curdir != wsDir:
+    os.chdir(wsDir)
 
 def main():
     model = models.resnet18(weights=None) 
@@ -29,12 +34,40 @@ def main():
     
     print(model) # Check last layer now
 
-    # Load CIFAR10 dataset
-    trainData = datasets.CIFAR10(root='data',
-                                 train=True,
-                                 download=True,
-                                 transform=ToTensor())
+    # %% Load CIFAR10 dataset from torchvision
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # Apply normalization
+
+    # Get dataset objects
+    train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+    validation_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+
+    # Define dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    validation_loader = DataLoader(validation_dataset, batch_size=64, shuffle=False)
+
+    # %% Define loss function and optimizer
+    initial_lr = 1E-4   
+
+    lossFcn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=initial_lr, fused=True)
+
+    options = {}    
+
+    dataloaderIndex = customTorchTools.dataloaderIndex(train_loader, validation_loader)
+
+    # Perform training and validation of model
+    customTorchTools.TrainAndValidateModel(dataloaderIndex, model, lossFcn, optimizer, options)
+    
+    # TODO: Update customTorchTool to support training of models for classification
+
+    with mlflow.start_run() as mlflow_run:
+        print('Start run: {name}'.format(name=mlflow_run.info.run_name))
+
+
+        mlflow.end_run()
+
 
 
 if __name__ == '__main__':
+    mlflow.set_experiment('ImageClassificationExample_CIFAR100')
     main()
